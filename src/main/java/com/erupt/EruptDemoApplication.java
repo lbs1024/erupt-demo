a2ac7d2fb535937fb0f9d1f72688ef5592635556
libaoshan <591366208@qq.com>
2022-05-30 16:12:18 +0800

    erupt-demo created!

---
+++ src/main/java/com/erupt/EruptDemoApplication.java
@@ -0,0 +1,22 @@
+package com.erupt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import xyz.erupt.core.annotation.EruptScan;
+
+/**
+* @Author LBS59
+* @Description SpringBoot项目核心启动类
+* @Date 15:07 2022/5/30
+**/
+@SpringBootApplication
+@EntityScan
+@EruptScan
+public class EruptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EruptDemoApplication.class, args);
+    }
+
+}
